6e1c8d33c6835645ed31ceb37519debbedff89f1
Ben Gosney <bengosney@googlemail.com>
2019-03-19 21:23:06 +0000

    Better propagation, better animation, better sizing

--- src/App.js
+++ src/App.js
@@ -8,20 +8,11 @@ class App extends Component {
 	super(props);
 
 	this.state = {
-	    pixelSize: 4,
-	    fontSize: 6,
-	    decay: 12,
-	    height: 50,
-	    width: 280,
-	    text: "Hello, World!"
+	    pixelSize: 6,
+	    height: 500,
+	    width: 150
 	};
 	
-	this.pixelSize = 4;
-	this.fontSize = 6;
-	this.decay = 12;
-	this.height = 50;
-	this.width = 280;
-
 	this.drawing = false;
 	this.fireState = [];
 
@@ -29,10 +20,13 @@ class App extends Component {
 
 	this.pallet = ['#070707', '#1f0707', '#2f0f07', '#470f07', '#571707', '#671f07', '#771f07', '#8f2707', '#9f2f07', '#af3f07', '#bf4707', '#c74707', '#DF4F07', '#DF5707', '#DF5707', '#D75F07', '#D7670F', '#cf6f0f', '#cf770f', '#cf7f0f', '#CF8717', '#C78717', '#C78F17', '#C7971F', '#BF9F1F', '#BF9F1F', '#BFA727', '#BFA727', '#BFAF2F', '#B7AF2F', '#B7B72F', '#B7B737', '#CFCF6F', '#DFDF9F', '#EFEFC7', '#FFFFFF'];
 	this.max = (this.pallet.length - 1);
+
+	this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
     }
 
     emptyFireState() {
-	const { height, width } = this.state;
+	const width = this.getWidth();
+	const height = this.getHeight();
 
 	const state = Array(height).fill().map(() => Array(width).fill(0));
 	state[0] = Array(width).fill(this.max);
@@ -42,45 +36,73 @@ class App extends Component {
 
     
     componentDidMount() {
-	const { height, width, pixelSize, decay } = this.state;
-	
 	const canvas = this.refs.canvas;
 	this.ctx = canvas.getContext("2d");
 
 	this.fireState = this.emptyFireState();
+	
+	this.rAF = requestAnimationFrame(() => this.updateAnimationState());
+	this.updateWindowDimensions();
+	window.addEventListener('resize', this.updateWindowDimensions);
+    }
+    
+    updateWindowDimensions() {
+	this.setState({ width: window.innerWidth, height: window.innerHeight });
+    }
+    
+    componentWillUnmount() {
+	cancelAnimationFrame(this.rAF);
+	window.removeEventListener('resize', this.updateWindowDimensions);
+    }
 
-	setInterval(() => this.updateFire(), 10);
-	//setTimeout(() => this.updateFire(), 100);
+    updateAnimationState() {
+	this.updateFire();
+	
+	this.rAF = requestAnimationFrame(() => this.updateAnimationState());
     }
 
     resetFire() {
 	this.fireState = this.emptyFireState();
     }
 
+    getWidth() {
+	const { width } = this.state;
+	const pixelSize = this.getPixelSize();
+	return Math.floor(width / pixelSize);
+    }
+    
+    getHeight() {
+	const { height } = this.state;
+	const pixelSize = this.getPixelSize();
+	return Math.floor(height / pixelSize);
+    }
+
+    getPixelSize() {
+	const { pixelSize } = this.state;
+	return Math.max(1, pixelSize);
+    }
+
     updateFire() {
-	const { height, width, decay } = this.state;
+	const width = this.getWidth();
+	const height = this.getHeight();
 
 	const newState = this.emptyFireState();
 	
 	for (let x = 0; x < width; x++) {
-	    for (let y = 0; y < height - 1; y++) {
-		const min = 5;
-		const max = 9;
-		
-		const mod = Math.random();
-		const wave = 0;
-		
+	    for (let y = 0; y < height - 1; y++) {	
 		const ay = y;
 		const ax = x;
-
-		const ny = ay + 1;
-		const nx = Math.min(Math.max(0, ax + Math.round(wave)), width);
-
+		
+		const ny = Math.max(ay + 1, 0);
+		const nx = Math.min(Math.max(ax, 0), width);
+		
 		try {
-		    newState[ny][nx] = Math.floor(this.fireState[ay][ax] * (1 - (mod.toPrecision(1) / Math.max(1, decay))));
+		    const pixel = this.fireState[ay][ax];
+		    const mod = Math.round(Math.random() * 3.0) & 3;
+		    
+		    newState[ny][nx] = Math.max(0, pixel - (mod & 1));
 		} catch(error) {
-		    console.error(error, ax, ay);
-		    return;
+		    //console.error(error, ax, ay, nx, ny);
 		}
 	    }
 	}
@@ -90,8 +112,10 @@ class App extends Component {
 	this.drawFire();
     }
 
-    drawFire() {
-	const { height, width, pixelSize, fontSize, text } = this.state;
+    drawFire() {	
+	const width = this.getWidth();
+	const height = this.getHeight();
+	const pixelSize = this.getPixelSize();
 	const { ctx } = this;
 	
 	ctx.fillStyle = this.pallet[0];
@@ -104,22 +128,12 @@ class App extends Component {
 		const ay = (height - y) - 1;
 		const ax = x;
 
-		try {
-		    if (this.fireState[ay][ax] !== 0) {
-			ctx.fillStyle = this.pallet[this.fireState[ay][ax]];
-			ctx.fillRect(x * pixelSize, y * pixelSize, pixelSize, pixelSize);
-		    }
-		} catch(error) {
-		    console.error(error, ax, ay);
-		    return;
+		if (this.fireState[ay][ax] !== 0) {
+		    ctx.fillStyle = this.pallet[this.fireState[ay][ax]];
+		    ctx.fillRect(x * pixelSize, y * pixelSize, pixelSize, pixelSize);
 		}
 	    }
 	}
-
-	ctx.font = `${fontSize * pixelSize}px Arial`;
-	ctx.fillStyle = "rgb(255,255,255)";
-	ctx.textAlign = "center";
-	ctx.fillText(text, (width * pixelSize) / 2, (height * pixelSize) / 2);
     }
 
     handleChange(name, event) {
@@ -144,12 +158,8 @@ class App extends Component {
 	
 	return (
 	    <div>
-	      { renderInput('pixelSize', pixelSize) }
-	      { renderInput('fontSize', fontSize) }
-	      { renderInput('decay', decay) }
-	      { renderInput('text', text) }
 	      <div>
-		<canvas ref="canvas" width={ width * pixelSize } height={ height * pixelSize } />
+		<canvas ref="canvas" width={ width } height={ height } />
 	      </div>
 	    </div>
 	);
